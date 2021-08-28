a67b2ad58f114b5c9c862f8963e3d18154f6488e
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-28 16:19:26 +0300

    feat: arriving meteorite

--- ArrivingMeteorite/ViewController.swift
+++ ArrivingMeteorite/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var planetImageView: UIImageView!
+    
+    let animator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configurationInitialSetup()
+        configureEndAnimations()
+    }
+    
+    func configurationInitialSetup() {
+        planetImageView.alpha = 0.0
+        planetImageView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    }
+    
+    func configureEndAnimations() {
+        animator.addAnimations {
+            self.planetImageView.alpha = 1.0
+            self.planetImageView.transform = CGAffineTransform(scaleX: 3, y: 3)
+        }
+        
+        animator.addAnimations({
+            self.planetImageView.frame.origin.y = -400
+        }, delayFactor: 0.6)
     }
 
-
+    @IBAction func valueDoidChange(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
+    }
+    
 }
 
